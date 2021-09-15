2a9fbf51edbaab794351aec1d038ab19a48efdeb
Martin Haiss <haissmartin@gmail.com>
2021-09-15 17:05:50 +0200

    deleted old files and cleaned up code

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,15 +1,4 @@
 import "./Cards.css";
-// import CardHermione from "../CardHermione/CardHermione";
-// import CardCedric from "../CardCedric/CardCedric";
-// import CardDraco from "../CardDraco/CardDraco";
-// import CardHarry from "../CardHarry/CardHarry";
-// import CardRon from "../CardRon/CardRon";
-// import harryPicture from "../img/harry.png";
-// import dracoPicture from "../img/draco.png";
-// import hermionePicture from "../img/hermione.png";
-// ((import cedricPicture from "../img/cedric.png";
-// import ronPicture from "../img/ron.png";
-
 import "./Cards.css";
 import data from "../data.json";
 import Card from "../Card/Card.js";
@@ -34,40 +23,3 @@ function Cards() {
 }
 
 export default Cards;
-
-/* ------ former "cards.js" before import via "data.json" --------
-
-
-function Cards() {
-  return (
-    <div className="Card--box">
-      <Card
-        characterName="Harry Potter"
-        house="Gryffindor"
-        imgUrl={harryPicture}
-      />
-      <Card
-        characterName="Draco Malfoy"
-        house="Slytherin"
-        imgUrl={dracoPicture}
-      />
-      <Card
-        characterName="Hermione Granger"
-        house="Gryffindor"
-        imgUrl={hermionePicture}
-      />
-      <Card
-        characterName="Cedric Diggory"
-        house="Hufflepuff"
-        imgUrl={cedricPicture}
-      />
-      <Card
-        characterName="Ron Weasley"
-        house="Gryffindor"
-        imgUrl={ronPicture}
-      />
-    </div>
-  );
-}
-
-export default Cards; */
