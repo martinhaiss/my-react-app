07034ece298d13b6aa978194360b31ade08c5065
Martin Haiss <haissmartin@gmail.com>
2021-09-15 22:47:49 +0200

    add effect for active footer-button

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,43 @@
+import { useState } from "react";
 import "./Footer.css";
 
 function Footer() {
+  const [activeHouse, setActiveHouse] = useState("");
   return (
     <footer className="Footer--box">
       <nav className="Footer--nav">
-        <button className="Footer--Button-Gr">Gr</button>
-        <button className="Footer--Button-Hu">Hu</button>
-        <button className="Footer--Button-Ra">Ra</button>
-        <button className="Footer--Button-Sl">SL</button>
+        <button
+          className={`${
+            activeHouse === "Gryffindor" ? "footer__button--active-gr" : ""
+          } Footer--Button-Gr`}
+          onClick={() => setActiveHouse("Gryffindor")}
+        >
+          Gr
+        </button>
+        <button
+          className={`${
+            activeHouse === "Hufflepuff" ? "footer__button--active-hu" : ""
+          } Footer--Button-Hu`}
+          onClick={() => setActiveHouse("Hufflepuff")}
+        >
+          Hu
+        </button>
+        <button
+          className={`${
+            activeHouse === "Ravenclaw" ? "footer__button--active-ra" : ""
+          } Footer--Button-Ra`}
+          onClick={() => setActiveHouse("Ravenclaw")}
+        >
+          Ra
+        </button>
+        <button
+          className={`${
+            activeHouse === "Slytherin" ? "footer__button--active-sl" : ""
+          } Footer--Button-Sl`}
+          onClick={() => setActiveHouse("Slytherin")}
+        >
+          Sl
+        </button>
       </nav>
     </footer>
   );
